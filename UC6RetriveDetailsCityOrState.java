20cfa9a39b9dcec89bb89cef597975c809eec203
PRUDHVI-9 <prudhvirajathelli@gmail.com>
2023-05-26 01:09:32 +0530

    Added:To retrieve a contact from address book

---
+++ UC6RetriveDetailsCityOrState.java
@@ -0,0 +1,17 @@
+mysql> select * from address_book;
++-----------+----------+------------------------+-----------+------------+--------+-------------+----------------------+
+| firstname | lastname | address                | city      | state      | zip    | phonenumber | email                |
++-----------+----------+------------------------+-----------+------------+--------+-------------+----------------------+
+| Hari      | Sharma   | plotno:99,Shiva colony | Pune      | Maharastra | 600009 |   723496788 | harisharma@gmail.com |
+| Krishna   | Kiran    | Surya colony           | Hyderabad | Telangana  | 600001 |   123456789 | saikiran@gmail.com   |
++-----------+----------+------------------------+-----------+------------+--------+-------------+----------------------+
+2 rows in set (0.00 sec)
+
+
+mysql> select * from address_book where city='Pune' or state='Maharastra';
++-----------+----------+------------------------+------+------------+--------+-------------+----------------------+
+| firstname | lastname | address                | city | state      | zip    | phonenumber | email                |
++-----------+----------+------------------------+------+------------+--------+-------------+----------------------+
+| Hari      | Sharma   | plotno:99,Shiva colony | Pune | Maharastra | 600009 |   723496788 | harisharma@gmail.com |
++-----------+----------+------------------------+------+------------+--------+-------------+----------------------+
+1 row in set (0.00 sec) 
